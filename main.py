aa5b916946a0cbf444124abdd5276cdef6b87634
champy0527 <138946854+champy0527@users.noreply.github.com>
2024-07-13 12:55:17 +0100

    Create a new Food class that inherits turtle. Make Food class go to a random location. On main.py, detect if distance between snake.head and food is close enough (maybe 10/15), then penup the original food, regenerate a new goto location.

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("The Old School Snake Game")
 screen.tracer(0)
-screen.listen()
-
 snake = Snake()
+food = Food()
+
+
+screen.listen()
 screen.onkey(fun=snake.up, key="Up")
 screen.onkey(fun=snake.down, key="Down")
 screen.onkey(fun=snake.left, key="Left")
@@ -22,6 +25,8 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    if snake.head.distance(food) <= 15:
+        food.generate_food()
     if snake.edge_of_screen():
         game_is_on = False
 
