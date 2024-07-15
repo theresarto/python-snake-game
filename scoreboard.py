a04702ed15b1856eb0c146ecd4a0d7352b68c779
champy0527 <138946854+champy0527@users.noreply.github.com>
2024-07-15 15:12:34 +0100

    Included a data.txt to store the high score of all time

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
 
+
 ALIGN = "center"
 FONT = ("Monospace", 18, "normal")
 
@@ -15,13 +16,14 @@ class Scoreboard(Turtle):
         self.goto(0, 260)
         self.color("white")
         self.speed("fastest")
-        self.current_high_score = 0
+        with open("data.txt") as data:
+            self.current_high_score = int(data.read())
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.clear()
         self.goto(0, 260)
-        self.write(f"Score: {self.score} | High Score: {self.current_high_score}", align=ALIGN, font=FONT)
+        self.write(f"       Score: {self.score}   |   High Score: {self.current_high_score}", align=ALIGN, font=FONT)
 
     def update_score(self):
         self.score += 1
@@ -37,10 +39,11 @@ class Scoreboard(Turtle):
         self.write("Press [SPACE] to start.", align=ALIGN, font=("Monospace", 24, "normal"))
         if self.score >= self.current_high_score:
             self.current_high_score = self.score
+            with open("data.txt", 'w') as data:
+                data.write(f"{self.current_high_score}")
         self.score = 0
         self.update_scoreboard()
 
     def press_space_to_start(self):
         self.goto(0, 0)
         self.write("Press [SPACE] to start.", align=ALIGN, font=("Monospace", 24, "normal"))
-
